285b2afd5d4ef4c36a683dc218aafb2a6ec9d272
Sascha Fleer <sascha.fleer@gmail.com>
2025-02-04 21:51:11 +0100

    build(docker): github actions aligned for docker build of UI

--- cistern_dashboard/src/app/ui/cards.tsx
+++ cistern_dashboard/src/app/ui/cards.tsx
@@ -1,80 +1,24 @@
-import {
-  RocketLaunchIcon,
-  ClockIcon,
-  CalendarIcon,
-  InboxIcon,
-} from "@heroicons/react/24/outline";
-import { fetchFillLevel } from "@/lib/data";
-
-const iconMap: { [key: string]: React.ComponentType } = {
-  current: RocketLaunchIcon,
-  month: CalendarIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
-};
+import { RocketLaunchIcon } from "@heroicons/react/24/outline";
 
 interface CardWrapperProps {
   fillLevel: number;
 }
 
 const CardWrapper: React.FC<CardWrapperProps> = ({ fillLevel }) => {
-  return (
-    <>
-      {fillLevel ? (
-        <Card title="Füllstand" value={fillLevel} type="current" />
-      ) : null}
-    </>
-  );
+  return <>{fillLevel ? <Card title="Füllstand" value={fillLevel} /> : null}</>;
 };
 
-// const {
-//   numberOfInvoices,
-//   numberOfCustomers,
-//   totalPaidInvoices,
-//   totalPendingInvoices,
-// } = await fetchCardData();
-//   let fillLevel = null;
-//   try {
-//     fillLevel = await fetchFillLevel();
-//   } catch (error) {
-//     console.error(error);
-//   }
-//
-//   return (
-//     <>
-//       {fillLevel ? (
-//         <Card title="Füllstand" value={fillLevel} type="current" />
-//       ) : null}
-//       {/* <Card title="Höchststand (Monat)" value={fillLevel} type="month" /> */}
-//       {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-//       {/* <Card */}
-//       {/*   title="Total Customers" */}
-//       {/*   value={numberOfCustomers} */}
-//       {/*   type="customers" */}
-//       {/* /> */}
-//     </>
-//   );
-// }
-
-// TODO: Cards with other information
 export function Card({
   title,
   value,
-  type,
 }: {
   title: string;
   value: number | string;
-  type: "invoices" | "customers" | "month" | "current";
 }): JSX.Element {
-  const Icon: React.ComponentType = iconMap[type];
-  console.log(type);
-
   return (
     <div className="rounded-xl bg-gray-50 dark:bg-gray-800 p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? (
-          <Icon className="h-5 w-5 text-gray-700 dark:text-gray-300" />
-        ) : null}
+        <RocketLaunchIcon className="h-5 w-5 text-gray-700 dark:text-gray-300" />
         <h3 className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
           {title}
         </h3>
