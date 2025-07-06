b45d139c0f8450016f29c2835763054be469dff3
semantic-release <semantic-release>
2025-07-06 20:57:28 +0000

    1.0.3
    Automatically generated by python-semantic-release

--- service/src/service/__init__.py
+++ service/src/service/__init__.py
@@ -21,7 +21,7 @@ LOG_DIR = "logs"
 LOG_CONFIG_DIR = os.path.join(os.path.dirname(__file__), "logging_config")
 
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 def __get_package_version() -> str:
